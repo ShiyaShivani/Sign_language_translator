3135bcef5b3fc8aea1c62f6531f5e4510677e2b9
Shiya Shivani <110982338+ShiyaShivani@users.noreply.github.com>
2025-06-01 16:55:55 +0530

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO('yolov8_weights/yolov8s.pt')
 model.train(
     data='assets/data.yaml',
-    epochs=25,
+    epochs=10,#you can do more for more accuracy
     imgsz=416,
     batch=32,
     name='sign_yolo_model',
